0a5b4ffd77c7c00f97ff497695e12e3eecee75d9
Sam <sam.w.neal@gmail.com>
2024-12-20 13:04:49 +1100

    Improve image object handling in BrotherPrintServer: Ensure imageObj is of type ImageObject before setting the image file, enhancing type safety and preventing potential runtime errors.

--- BrotherPrintServer/Brother.cs
+++ BrotherPrintServer/Brother.cs
@@ -492,14 +492,14 @@ namespace BrotherPrintServer
 				{
 					// Find and replace Object0 if it exists
 					IObject imageObj = doc.GetObject("Object0");
-					if (imageObj != null)
+					if (imageObj != null && imageObj is ImageObject)
 					{
 						// Save temp image file
 						string tempImagePath = Path.GetTempFileName() + ".bmp";
 						image.Save(tempImagePath, System.Drawing.Imaging.ImageFormat.Bmp);
 
 						// Replace image in template
-						imageObj.SetImageFile(tempImagePath);
+						((ImageObject)imageObj).SetImageFile(tempImagePath);
 
 						// Clean up temp file
 						try { File.Delete(tempImagePath); } catch { }
